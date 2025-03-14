9b7650d83598f25a1414126153355e2fcec0fe1a
xphip <xphipme@gmail.com>
2025-03-14 05:20:55 -0300

    chore: simplifies the cli by delegating control flow

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,11 +1,37 @@
 package main
 
-import "test-hub-golang/pkg/cli"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"test-hub-golang/pkg/eventhub"
+)
+
+func Consume(consumer eventhub.Consumer) {
+	for event := range consumer {
+		fmt.Println(event)
+	}
+}
 
 func main() {
+	wait := make(chan os.Signal, 1)
+	signal.Notify(wait, syscall.SIGINT, syscall.SIGTERM)
 
-	app := cli.NewCli()
-	if errRun := app.Run(); errRun != nil {
-		panic(errRun)
+	eventHub := eventhub.New()
+
+	for i := 0; i < 2; i++ {
+		consumer := eventHub.Subscribe()
+		go Consume(consumer)
 	}
+
+	for i := 1; i <= 5; i++ {
+		event := fmt.Sprintf("Evento %d", i)
+		eventHub.Publish(event)
+	}
+
+	eventHub.Close()
+
+	<-wait
+	fmt.Println("EventHub finished")
 }
