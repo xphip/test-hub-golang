e191c4842cf1c1ab9b6a35d073f54dc5f669dc37
xphip <xphipme@gmail.com>
2025-03-14 07:34:22 -0300

    feat: add unit test

---
+++ pkg/eventhub/eventhub_test.go
@@ -0,0 +1,32 @@
+package eventhub_test
+
+import (
+	"sync"
+	"test-hub-golang/pkg/eventhub"
+	"testing"
+)
+
+func TestSubscribeAndPublish(t *testing.T) {
+	var (
+		wg                = sync.WaitGroup{}
+		eventHub          = eventhub.New()
+		consumer          = eventHub.Subscribe()
+		eventTest         = "This is and event test"
+		eventTestReceived string
+	)
+
+	wg.Add(1)
+
+	go func(g *sync.WaitGroup, id int, c eventhub.Consumer) {
+		defer g.Done()
+		eventTestReceived = <-c
+	}(&wg, 0, consumer)
+
+	eventHub.Publish(eventTest)
+
+	wg.Wait()
+
+	if eventTest != eventTestReceived {
+		t.Error("event received doesnt match with espected event")
+	}
+}
